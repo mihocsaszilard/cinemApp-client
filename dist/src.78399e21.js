e7e8e401d9695848c605313c6f6b97011dad118a
Mihocsa Szilard <mihocsa48@gmail.com>
2021-07-19 21:12:38 +0200

    changing websocket

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30007,7 +30007,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52657" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54930" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
